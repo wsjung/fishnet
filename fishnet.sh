4b6d348464b1e940af76e73609d8d9f29ba879eb
Woo Jung <wooseokjung0826@gmail.com>
2025-01-12 03:50:40 -0600

    Phase2 step2 (#7)
    * added stage 2 step 2 script: dc_generate_rp_statistics.py. edited proper filepath processing
    
    * added temporary file with the threshold-network pairs for dc_generate_rp.sbatch
    
    * phase 2 step 2 complete
    
     - works for temporary input file test/slurm_thresholds_maleWC.txt
     - added prints to stdout for each step
     - removed prints from step 2 python script

--- fishnet.sh
+++ fishnet.sh
@@ -21,116 +21,127 @@ output_dir=$( readlink -f ./results/ )
 #   TODO: add to biocontainers 
 container_python="jungwooseok/dc_rp_genes:1.0"
 
-################
-#### PHASE 1 ###
-################
-## (1) nextflow original run
-#echo "executing Nextflow MEA pipeline on original run"
-#nextflow run ./scripts/phase1/nextflow/main.nf \
-#    --trait $trait \
-#    --moduleFileDir $moduleFileDir \
-#    --numTests $numTests \
-#    --pipeline $trait  \
-#    --pvalFileName $pvalFileName \
-#    --geneColName $geneColName \
-#    --pvalColName $pvalColName  \
-#    --bonferroni_alpha $bonferroni_alpha \
-#    -c conf/fishnet.config
-#
-#
-## (2) generate uniform p-values
-#echo "generating uniformly distributed p-values"
-#genes_filepath="/app/${pvalFileName#$(pwd)}"
-#docker run -i --rm -v $(pwd):/app -u $(id -u):$(id -g) $container_python  /bin/bash -c \
-#    "python3 /app/scripts/phase1/generate_uniform_pvals.py \
-#       --genes_filepath $genes_filepath"
-#echo "done"
-#
-#
-## (3) nextflow random permutation run
-#echo "executing Nextflow MEA pipeline on random permutations"
-#pvalFileName=$( readlink -f ./test/${trait}RR/0-${trait}RR.csv )
-#num_permutations="1"
-#nextflow run ./scripts/phase1/nextflow/main.nf \
-#    --trait "${trait}RR" \
-#    --moduleFileDir $moduleFileDir \
-#    --numTests $numTests \
-#    --pipeline "${trait}RR"  \
-#    --pvalFileName $pvalFileName \
-#    --geneColName $geneColName \
-#    --pvalColName $pvalColName  \
-#    --bonferroni_alpha $bonferroni_alpha \
-#    --random_permutation \
-#    --numRP $num_permutations \
-#    --GO_summaries_path "GO_summaries_RP" \
-#    --masterSummaries_path "masterSummaries_RP" \
-#    -c conf/fishnet.config \
-#
-## (4) compile results
-## (4.1) original
-#echo "compiling results"
-#results_path="/app/${output_dir#$(pwd)}"
-#summaries_path_original="${results_path}/masterSummaries/summaries/"
-#docker run -i --rm -v $(pwd):/app -u $(id -u):$(id -g) $container_python /bin/bash -c \
-#    "python3 /app/scripts/phase1/compile_results.py \
-#        --dirPath $summaries_path_original \
-#        --identifier $trait \
-#        --output $results_path"
-#
-## (4.2) random permutation
-#summaries_path_permutation="${results_path}/masterSummaries_RP/summaries/"
-#docker run -i --rm -v $(pwd):/app -u $(id -u):$(id -g) $container_python /bin/bash -c \
-#    "python3 /app/scripts/phase1/compile_results.py \
-#        --dirPath $summaries_path_permutation \
-#        --identifier ${trait}RR \
-#        --output $results_path"
-#echo "done"
-#
-## (5) organize phase 1 results
-#echo "restructuring phase 1 results for input to phase 2"
-## TODO: consider defining shell functions in separate file
-## function to organize phase 1 nextflow pipeline results
-#organize_nextflow_results() {
-#    trait=$1
-#    results_dir=$2
-#    traitRR="${trait}RR"
-#
-#    # create outer trait directories
-#    trait_dir="${results_dir}/${trait}/"
-#    traitRR_dir="${results_dir}/${traitRR}/"
-#    mkdir -p $trait_dir $traitRR_dir
-#
-#    # move over original results
-#    mv "${results_dir}/GO_summaries" ${trait_dir}
-#    mv "${results_dir}/masterSummaries" ${trait_dir}
-#    mv "${results_dir}/master_summary_${trait}.csv" ${trait_dir}/master_summary.csv
-#
-#    # 3. move over permutation results
-#    mv "${results_dir}/GO_summaries_RP" "${traitRR_dir}/GO_summaries"
-#    mv "${results_dir}/masterSummaries_RP" "${traitRR_dir}/masterSummaries"
-#    mv "${results_dir}/master_summary_${traitRR}.csv" ${traitRR_dir}/master_summary.csv
-#}
-#organize_nextflow_results $trait $output_dir
-#echo "done"
+###############
+### PHASE 1 ###
+###############
+echo "
+###############
+### PHASE 1 ###
+###############
+"
+# (1) nextflow original run
+echo "# STEP 1.1: executing Nextflow MEA pipeline on original run"
+nextflow run ./scripts/phase1/nextflow/main.nf \
+    --trait $trait \
+    --moduleFileDir $moduleFileDir \
+    --numTests $numTests \
+    --pipeline $trait  \
+    --pvalFileName $pvalFileName \
+    --geneColName $geneColName \
+    --pvalColName $pvalColName  \
+    --bonferroni_alpha $bonferroni_alpha \
+    -c conf/fishnet.config
+
+
+# (2) generate uniform p-values
+echo "# STEP 1.2: generating uniformly distributed p-values"
+genes_filepath="/app/${pvalFileName#$(pwd)}"
+docker run -i --rm -v $(pwd):/app -u $(id -u):$(id -g) $container_python  /bin/bash -c \
+    "python3 /app/scripts/phase1/generate_uniform_pvals.py \
+       --genes_filepath $genes_filepath"
+echo "done"
+
+
+# (3) nextflow random permutation run
+echo "# STEP 1.3: executing Nextflow MEA pipeline on random permutations"
+pvalFileName=$( readlink -f ./test/${trait}RR/0-${trait}RR.csv )
+num_permutations="1"
+nextflow run ./scripts/phase1/nextflow/main.nf \
+    --trait "${trait}RR" \
+    --moduleFileDir $moduleFileDir \
+    --numTests $numTests \
+    --pipeline "${trait}RR"  \
+    --pvalFileName $pvalFileName \
+    --geneColName $geneColName \
+    --pvalColName $pvalColName  \
+    --bonferroni_alpha $bonferroni_alpha \
+    --random_permutation \
+    --numRP $num_permutations \
+    --GO_summaries_path "GO_summaries_RP" \
+    --masterSummaries_path "masterSummaries_RP" \
+    -c conf/fishnet.config \
+
+# (4) compile results
+echo "# STEP 4: compiling results"
+# (4.1) original
+echo "# STEP 4.1: original results"
+results_path="/app/${output_dir#$(pwd)}"
+summaries_path_original="${results_path}/masterSummaries/summaries/"
+docker run -i --rm -v $(pwd):/app -u $(id -u):$(id -g) $container_python /bin/bash -c \
+    "python3 /app/scripts/phase1/compile_results.py \
+        --dirPath $summaries_path_original \
+        --identifier $trait \
+        --output $results_path"
+
+# (4.2) random permutation
+echo "# STEP 4.2: permutation results"
+summaries_path_permutation="${results_path}/masterSummaries_RP/summaries/"
+docker run -i --rm -v $(pwd):/app -u $(id -u):$(id -g) $container_python /bin/bash -c \
+    "python3 /app/scripts/phase1/compile_results.py \
+        --dirPath $summaries_path_permutation \
+        --identifier ${trait}RR \
+        --output $results_path"
+echo "done"
+
+# (5) organize phase 1 results
+echo "# STEP 5: restructuring phase 1 results for input to phase 2"
+# TODO: consider defining shell functions in separate file
+# function to organize phase 1 nextflow pipeline results
+organize_nextflow_results() {
+    trait=$1
+    results_dir=$2
+    traitRR="${trait}RR"
+
+    # create outer trait directories
+    trait_dir="${results_dir}/${trait}/"
+    traitRR_dir="${results_dir}/${traitRR}/"
+    mkdir -p $trait_dir $traitRR_dir
+
+    # move over original results
+    mv "${results_dir}/GO_summaries" ${trait_dir}
+    mv "${results_dir}/masterSummaries" ${trait_dir}
+    mv "${results_dir}/master_summary_${trait}.csv" ${trait_dir}/master_summary.csv
+
+    # 3. move over permutation results
+    mv "${results_dir}/GO_summaries_RP" "${traitRR_dir}/GO_summaries"
+    mv "${results_dir}/masterSummaries_RP" "${traitRR_dir}/masterSummaries"
+    mv "${results_dir}/master_summary_${traitRR}.csv" ${traitRR_dir}/master_summary.csv
+}
+organize_nextflow_results $trait $output_dir
+echo "done"
 
 
 ###############
 ### PHASE 2 ###
 ###############
-# (0) TODO: filter the summary file for original/permuted runs
-#   - filter for 'True' in master_summary.csv -> master_summary_filtered.csv
-#   - subset columns of master_summary_filtered.csv -> master_summary_filtered_parsed.csv
+echo "
+###############
+### PHASE 2 ###
+###############
+"
+# (0) filter the summary file for original/permuted runs
+echo "# STEP 2.0: filtering + parsing master_summary.csv files"
 ( head -n 1 "${output_dir}/${trait}/master_summary.csv"; grep 'True' "${output_dir}/${trait}/master_summary.csv" ) > "${output_dir}/${trait}/master_summary_filtered.csv"
 cut -d ',' -f 1-8 "${output_dir}/${trait}/master_summary_filtered.csv" > "${output_dir}/${trait}/master_summary_filtered_parsed.csv"
 
 ( head -n 1 "${output_dir}/${trait}RR/master_summary.csv"; grep 'True' "${output_dir}/${trait}RR/master_summary.csv" ) > "${output_dir}/${trait}RR/master_summary_filtered.csv"
 cut -d ',' -f 1-8 "${output_dir}/${trait}RR/master_summary_filtered.csv" > "${output_dir}/${trait}RR/master_summary_filtered_parsed.csv"
 
-# (1) generate statistics for original run
+## (1) generate statistics for original run
+echo "# STEP 2.1: generating statistics for original run"
 genes_filedir="/app/${pvalFileDir#$(pwd)}"
 module_filedir="/app/${moduleFileDir#$(pwd)}"
 results_dir="/app/${output_dir#$(pwd)}"
-echo "${results_dir}/${trait}/master_summary_filtered_parsed.csv"
 for network in `ls ${moduleFileDir}`;
 do
     docker run -i --rm -v $(pwd):/app -u $(id -u):$(id -g) $container_python /bin/bash -c \
@@ -144,4 +155,29 @@ do
             --output_path ${results_dir}/${trait}/results/raw/ \
             --network $network"
 done
+ehco "done"
 
+# (2) generate statistics for permutation run
+# TODO: rewrite in nextflow(?) for parallelism
+# (2.1) TODO: prepare file with threshold:network pairs
+
+threshold_network_pairs=$( readlink -f ./test/slurm_thresholds_maleWC.txt )
+# (2.2) generate statistics for permutation run
+echo "# STEP 2.2: generating statistics for permutation runs"
+genes_rpscores_filedir="/app/results/RPscores/${trait}RR/"
+while IFS=$'\t' read -r threshold network; do
+    echo "Threshold $threshold, Network: $network"
+    docker run --rm -v $(pwd):/app -u $(id -u):$(id -g) $container_python /bin/bash -c \
+        "python3 /app/scripts/phase2/dc_generate_rp_statistics.py \
+            --gene_set_path $genes_rpscores_filedir \
+            --master_summary_path ${results_dir}/${trait}RR/master_summary_filtered_parsed.csv \
+            --trait ${trait}RR \
+            --module_path ${module_filedir}/${network}.txt \
+            --go_path ${results_dir}/${trait}RR/GO_summaries/${trait}RR/ \
+            --output_path ${results_dir}/${trait}RR/results/raw/ \
+            --network $network \
+            --threshold $threshold"
+done < $threshold_network_pairs
+echo "done"
+
+echo "### FISHNET COMPLETE ###"
