d3aed9e503e0db2a5c1369fa400af10fd853856f
Woo Jung <jungw@wustl.edu>
2025-09-24 22:59:50 -0500

    update with example scripts in test/.

---
+++ test/example_malWC_docker.sh
@@ -0,0 +1,14 @@
+study="./data/pvals/exampleOR/"
+study_random="./data/pvals/exampleRR/"
+modules="./data/modules/ker_based/"
+num_permutations=500
+
+./fishnet_multi.sh \
+    --study $study \
+    --study-random $study_random \
+    --modules $modules \
+    --docker \
+    --conda \
+    --conda_env fishnet \
+    --num-permutations $num_permutations
+
